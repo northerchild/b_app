797cee346651279f0552ab4b7f0a5cc2d83d3d70
northerchild <osarus-13@hotmail.com>
2019-11-08 11:27:58 -0500

    archivos base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div>
+      <h1>hola</h1>
+    </div>
+  );
+}
+
+export default App;
