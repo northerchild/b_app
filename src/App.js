0ffa367a067a7546c57ba58d30b393990ea0ad07
northerchild <osarus-13@hotmail.com>
2019-11-08 12:08:35 -0500

    Se hace la consulta a la API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
-
+import axios from 'axios';
 
 function App() {
+  const [frase, obtenerFrase] = useState({});
+  const consultarAPI = async()=>{
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    // Agregar el resultado de la API al state; (similar a this.setState)
+    obtenerFrase(resultado.data[0])
+  }
+    //consulta a una rest Api
+  useEffect(
+    () =>{
+      consultarAPI();
+    },[]
+  );
   return (
     <div>
-      <h1>hola</h1>
+
     </div>
   );
 }
